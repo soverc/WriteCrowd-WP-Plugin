ea74a41fcfa0368b6950196a4f5a070ab9f18917
mark <mark@linux-xj50.site>
2011-09-15 03:35:42 -0400

    Don't exlcude oneighty.php file accidentally when trying to exclude oneighty/oneighty*

--- pack.sh
+++ pack.sh
@@ -1,4 +1,5 @@
 PACKNAME="oneighty"
+VERSION="0.98c"
 ln -s . $PACKNAME
-zip  -x "$PACKNAME/$PACKNAME" "$PACKNAME/$PACKNAME*" "$PACKNAME/.git*" "$PACKNAME/classes/xmlrpc/test*" "$PACKNAME/classes/xmlrpc/demo*" "$PACKNAME/classes/xmlrpc/doc*" "$PACKNAME/writecrowd_oneighty.zip" "$PACKNAME/pack.sh" -r writecrowd_oneighty.zip $PACKNAME
+zip  -x "$PACKNAME/$PACKNAME" "$PACKNAME/$PACKNAME/*" "$PACKNAME/.git*" "$PACKNAME/classes/xmlrpc/test*" "$PACKNAME/classes/xmlrpc/demo*" "$PACKNAME/classes/xmlrpc/doc*" "$PACKNAME/writecrowd_oneighty.zip" "$PACKNAME/pack.sh" -r writecrowd_oneighty_$VERSION.zip $PACKNAME
 rm $PACKNAME
